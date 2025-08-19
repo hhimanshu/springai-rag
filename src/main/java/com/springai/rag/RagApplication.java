c004c479f02cf11a58e78b100ff1ba8b1e7d3157
hhimanshu <dev.atwork@yahoo.com>
2025-08-19 12:20:05 +1200

    Update OpenAI API key placeholder in application.properties

--- src/main/java/com/springai/rag/RagApplication.java
+++ src/main/java/com/springai/rag/RagApplication.java
@@ -1,14 +1,37 @@
 package com.springai.rag;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RagApplication {
 
+	private final ChatClient chatClient;
+
+	public RagApplication(ChatClient.Builder chatBuilder) {
+		this.chatClient = chatBuilder.build();
+	}
+
+	void makeLLMCall() {
+		String response = this.chatClient.prompt().user("Who are you!").call().content();
+		System.out.println("LLM Response: " + response);
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			makeLLMCall();
+		};
+	}
 	public static void main(String[] args) {
 		System.out.println("Application Setup ... OK");
-		SpringApplication.run(RagApplication.class, args);
+		SpringApplication app = new SpringApplication(RagApplication.class);
+		app.setWebApplicationType(WebApplicationType.NONE);
+		app.run(args);
 	}
 
 }
